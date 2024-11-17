2928322100e9ac88b086484db0f0d27dd2ec8f40
FRANCIS RONIE SA DA SILVA <null>
2024-11-17 20:23:56 -0300

    Encrypt de credenciais

--- engenharia_dados/carga_inicial_dados.py
+++ engenharia_dados/carga_inicial_dados.py
@@ -3,7 +3,7 @@ import pymongo
 import datetime
 from faker import Faker
 
-client = pymongo.MongoClient(r"mongodb+srv://pi2_writer:74ZMHoV9iSZtMrfr@cluster0.tz7tehv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+client = pymongo.MongoClient(r"mongodb+srv://secret:secret@cluster0.tz7tehv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 db = client["proj_integrador_ii"]
 collection = db["perm_trabalho"]
 
--- engenharia_dados/extrator_bi_acidentes.py
+++ engenharia_dados/extrator_bi_acidentes.py
@@ -2,7 +2,7 @@
 import pymongo
 import pandas as pd
 
-client = pymongo.MongoClient('mongodb+srv://pi2_writer:74ZMHoV9iSZtMrfr@cluster0.tz7tehv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
+client = pymongo.MongoClient('mongodb+srv://secret:secret@cluster0.tz7tehv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
 db = client['proj_integrador_ii']
 collection = db['historico_acidente']
 
